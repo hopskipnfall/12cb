2d6961d05fa14bb2eedf1d966e8774cccd55cd7a
Jonn Spencer <jonn@jonnjonnjonn.com>
2019-08-01 21:34:52 +0900

    Remove CharacterPicker module (unused).
    Also sort stuff in app.module.ts.

--- jyuunikyara/src/app/app.module.ts
+++ jyuunikyara/src/app/app.module.ts
@@ -1,70 +1,65 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CharacterGridComponent } from './character-grid/character-grid.component';
-import { CharacterComponent } from './character/character.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { SetupComponent } from './setup/setup.component';
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { CharacterPickerComponent } from './character-picker/character-picker.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { AppRoutingModule } from './app-routing.module';
 import { ArenaComponent } from './arena/arena.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CharacterComponent } from './character/character.component';
+import { CharacterGridComponent } from './character-grid/character-grid.component';
+import { CookieService } from 'ngx-cookie-service';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatRippleModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { CookieService } from 'ngx-cookie-service';
-import { WinscreenComponent } from './winscreen/winscreen.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { NgModule } from '@angular/core';
+import { SetupComponent } from './setup/setup.component';
+import { WinscreenComponent } from './winscreen/winscreen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterGridComponent,
-    CharacterComponent,
-    SetupComponent,
-    CharacterPickerComponent,
     ArenaComponent,
+    CharacterComponent,
+    CharacterGridComponent,
+    SetupComponent,
     WinscreenComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatDividerModule,
     MatFormFieldModule,
     MatGridListModule,
-    MatInputModule,
-    MatButtonModule,
-    FormsModule,
-    MatCardModule,
-    MatDividerModule,
-    MatToolbarModule,
     MatIconModule,
-    MatDialogModule,
-    MatChipsModule,
+    MatInputModule,
     MatRippleModule,
     MatSelectModule,
     MatSidenavModule,
-    MatTooltipModule,
     MatSnackBarModule,
+    MatToolbarModule,
+    MatTooltipModule,
   ],
   providers: [
     CookieService,
   ],
   bootstrap: [AppComponent],
-  entryComponents: [
-    CharacterPickerComponent,
-  ]
+  entryComponents: []
 })
 export class AppModule { }
